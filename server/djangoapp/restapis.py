8166fb5dea6f8985e9240ed2360ffb902c648eb7
elaine1129 <elainelowjingyi00@gmail.com>
2026-03-19 16:18:46 -0400

    fix lint errors

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -15,6 +15,7 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
