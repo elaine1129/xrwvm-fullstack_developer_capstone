f560ff51be1855c1b04becdd0ad9ab49af19ffb3
elaine1129 <elainelowjingyi00@gmail.com>
2026-03-19 16:01:21 -0400

    fix lint error

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -40,11 +39,7 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
 
     def __str__(self):
         return self.name  # Return the name as the string representation
